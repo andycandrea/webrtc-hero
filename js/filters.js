f9d92c7bb473b63ca4097f41079798ce69cfd8a1
Andy Andrea <andy.andrea@scimedsolutions.com>
2014-12-19 13:55:44 -0500

    Preliminary video chat functionality

---
+++ js/filters.js
@@ -0,0 +1,9 @@
+function toggleLocalFilter(klass) {
+  var video = $('#local-video');
+  video.toggleClass(klass);
+}
+
+function toggleRemoteFilter(klass) {
+  var video = $('#remote-video');
+  video.toggleClass(klass);
+}
