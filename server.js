ccc3b9321a4ff4b927df89a1ebe198c84eb5fa61
Andy Andrea <andy.andrea@scimedsolutions.com>
2015-03-20 11:13:43 -0400

    Set port to use Heroku's default

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ var http = require('http');
 var socketIO = require('socket.io');
 
 var fileServer = new(nodeStatic.Server)();
+var port = process.env.PORT || 2014
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(443);
+}).listen(port);
 
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
